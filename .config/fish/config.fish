51dc246bddf88ad26f5c5f26e7e77e72b9d05bbc
Austin <austinlai2112@gmail.com>
2021-10-24 21:34:37 +1100

    Cleared out for refresh

--- .config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-alias tmuxa="tmux a -t"
-
-set -xU MANPAGER 'less -R --use-color -Dd+r -Du+b'
--- README.md
+++
@@ -1,4 +0,0 @@
-# dotfiles
-Bunch of unorganised dotfiles
-
-- Added to git  in the dumbest way - pushed straight from home directory
