ff753f09da6ca3a435a18177cfb8277ec8d7d515
Austin <austinlai2112@gmail.com>
2023-01-28 21:11:55 +1100

    Added gitignore for neovim packer compiled

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,17 +2,17 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
+# If all your bars have ipc enabled, you can also use
 #polybar-msg cmd quit
-# 
+#
 # Launch bar1 and bar2
 # cho "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 # polybar base 2>&1 | tee -a /tmp/polybar1.log & disown
 
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload wpgtk &
+        MONITOR=$m polybar --reload &
     done
 else
-    polybar --reload wpgtk &
+    polybar --reload &
 fi
